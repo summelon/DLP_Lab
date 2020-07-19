a30a4259b5049baa67d9fdd0930b1ffaf4856335
summelon <summelon7@gmail.com>
2020-07-19 16:35:40 +0800

    Add momentum

--- lab1/model.py
+++ lab1/model.py
@@ -1,11 +1,16 @@
+import copy
 import numpy as np
+
 import utils
 
 
 class Linear:
     def __init__(self, in_features: int, out_features: int, bias=False):
         self.weight = np.random.normal(size=(in_features, out_features))
-        self.weight_grad = 0
+        self.weight_ahead = copy.deepcopy(self.weight)
+
+        self.weight_grad = np.zeros_like(self.weight)
+        self.velocity = copy.deepcopy(self.weight_grad)
         if bias:
             self.bias = np.random.normal(size=out_features)
             self.bias_grad = 0
@@ -23,8 +28,10 @@ class Linear:
         else:
             return np.dot(inp, prev_grad), np.dot(np.ones(len(inp)), prev_grad)
 
-    def update(self, lr: float):
-        self.weight -= lr * self.weight_grad
+    def update(self, lr: float, gamma: float = 0):
+        self.velocity = lr * self.weight_grad + gamma * self.velocity
+        self.weight -= self.velocity
+        self.weight_ahead = self.weight + gamma * self.velocity
         if self.bias is not None:
             self.bias -= lr * self.bias_grad
 
@@ -32,7 +39,8 @@ class Linear:
 class MyModel:
     def __init__(self, in_features: int, out_features: int, hid_dim: list):
         self.linear_1 = Linear(in_features, hid_dim[0])
-        self.linear_2 = Linear(hid_dim[0], out_features)
+        self.linear_2 = Linear(hid_dim[0], hid_dim[1])
+        self.linear_3 = Linear(hid_dim[1], out_features)
         self.params = dict()
 
     def forward(self, inp: np.ndarray):
@@ -41,24 +49,38 @@ class MyModel:
         self.params['A1'] = self.sigmoid(self.params['Z1'])
         self.params['Z2'] = self.linear_2.forward(self.params['A1'])
         self.params['A2'] = self.sigmoid(self.params['Z2'])
+        self.params['Z3'] = self.linear_3.forward(self.params['A2'])
+        self.params['A3'] = self.sigmoid(self.params['Z3'])
 
-        return self.params['A2']
+        return self.params['A3']
 
-    def backward(self, inp: np.ndarray, otpt: np.ndarray, gd: np.ndarray):
-        w2_prev_grad = np.multiply(
+    def backward(self, inp: np.ndarray, otpt: np.ndarray, gd: np.ndarray,
+                 nesterov: bool = True):
+        w3_prev_grad = np.multiply(
                 self.mse(otpt, gd, derivative=True),
+                self.sigmoid(self.params['Z3'], derivative=True))
+        self.linear_3.weight_grad = self.linear_3.backward(
+                self.params['A2'], w3_prev_grad)
+
+        w2_prev_grad = np.multiply(
+                np.dot(w3_prev_grad,
+                       self.linear_3.weight_ahead.T
+                       if nesterov else self.linear_3.weight.T),
                 self.sigmoid(self.params['Z2'], derivative=True))
         self.linear_2.weight_grad = self.linear_2.backward(
                 self.params['A1'], w2_prev_grad)
 
         w1_prev_grad = np.multiply(
-                np.dot(w2_prev_grad, self.linear_2.weight.T),
+                np.dot(w2_prev_grad,
+                       self.linear_2.weight_ahead.T
+                       if nesterov else self.linear_2.weight.T),
                 self.sigmoid(self.params['Z1'], derivative=True))
-        self.linear_1.weight_grad = self.linear_1.backward(inp, w1_prev_grad)
+        self.linear_1.weight_grad = self.linear_1.backward(
+                inp, w1_prev_grad)
 
-    def update(self, lr=1e-3):
-        self.linear_1.update(lr)
-        self.linear_2.update(lr)
+    def update(self, lr=1e-3, gamma=9e-1):
+        self.linear_1.update(lr, gamma)
+        self.linear_2.update(lr, gamma)
 
     # Activation function
     def sigmoid(self, inp: np.ndarray, derivative=False):
@@ -88,20 +110,24 @@ class MyModel:
 
 
 def main():
-    model = MyModel(in_features=2, out_features=1, hid_dim=[8])
-    # inputs, labels = utils.generate_linear()
-    inputs, labels = utils.generate_xor_easy()
+    np.random.seed(7)
+    model = MyModel(in_features=2, out_features=1, hid_dim=[128, 256])
+    inputs, labels = utils.generate_linear(n=10000)
+    # inputs, labels = utils.generate_xor_easy()
 
     for e in range(100000):
         outputs = model.forward(inputs)
-        model.backward(inputs, outputs, labels)
-        model.update()
-        if e % 1000 == 0:
+        model.backward(inputs, outputs, labels, nesterov=False)
+        model.update(lr=1e-5, gamma=9e-1)
+        if e % 1 == 0:
             preds = model.pred(outputs)
             loss = model.mse(outputs, labels).mean()
             print(f"[{e:^7d}]Accuracy now is "
                   f"{model.cal_acc(preds, labels):.2%}"
                   f", Loss now is {loss:.2f}",)
+            if model.cal_acc(preds, labels) == 1:
+                print("Achieved 100% accuracy")
+                break
 
 
 if __name__ == "__main__":
