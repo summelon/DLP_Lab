946d3879982fe46981ce6412739e8aed86b3a17c
summelon <summelon7@gamil.com>
2020-07-22 08:49:54 +0800

    Add BCE, revise MSE

--- lab1/model.py
+++ lab1/model.py
@@ -55,10 +55,17 @@ class MyModel:
         return self.params['A3']
 
     def backward(self, inp: np.ndarray, otpt: np.ndarray, gd: np.ndarray,
-                 nesterov: bool = True):
-        w3_prev_grad = np.multiply(
-                self.mse(otpt, gd, derivative=True),
-                self.sigmoid(self.params['Z3'], derivative=True))
+                 nesterov: bool = True, loss='ce'):
+        if loss == 'ce':
+            w3_prev_grad = np.multiply(
+                    self.mse(otpt, gd, derivative=True),
+                    self.sigmoid(self.params['Z3'], derivative=True))
+        elif loss == 'mse':
+            w3_prev_grad = np.multiply(
+                    self.ce(otpt, gd, derivative=True),
+                    self.sigmoid(self.params['Z3'], derivative=True))
+        else:
+            raise ValueError(f"No such loss function: {loss}")
         self.linear_3.weight_grad = self.linear_3.backward(
                 self.params['A2'], w3_prev_grad)
 
@@ -93,11 +100,18 @@ class MyModel:
 
     # Loss function
     # TODO 1.multi output 2.check if output is one-hot encoding
-    def mse(self, otpt: np.ndarray, gd: np.ndarray, derivative=False):
+    def mse(self, otpt: np.ndarray, gt: np.ndarray, derivative=False):
         if derivative:
-            return 2 * (otpt-gd)
+            return 2 * (otpt-gt) / len(otpt)
         else:
-            return 0.5 * ((gd-otpt)**2 + (otpt-gd)**2)
+            return 0.5 * np.mean(((gt-otpt)**2 + (otpt-gt)**2))
+
+    # Cross entropy
+    def ce(self, otpt: np.ndarray, gt: np.ndarray, eps=1e-8, derivative=False):
+        if derivative:
+            return (((1-gt) / (1-otpt+eps)) - (gt/otpt+eps)) / len(otpt)
+        else:
+            return -np.mean(gt*np.log(otpt+eps) + (1-gt)*np.log(1-otpt+eps))
 
     def pred(self, otpt):
         assert all(e <= 1 and e >= 0 for e in otpt), "Value(s) is wrong!"
@@ -112,22 +126,39 @@ class MyModel:
 def main(params):
     np.random.seed(7)
     model = MyModel(in_features=2, out_features=1, hid_dim=[8, 16])
-    inputs, labels = utils.generate_linear(n=10000)
-    # inputs, labels = utils.generate_xor_easy()
-    print(params['show_period'])
+    if params['data'] == 'linear':
+        inputs, labels = utils.generate_linear(n=100)
+    elif params['data'] == 'xor':
+        inputs, labels = utils.generate_xor_easy()
+    else:
+        raise KeyError("Please select a correct input data")
 
-    for e in range(100000):
+    # Start training
+    e = 0
+    loss_recorder = []
+    while(1):
+        e += 1
         outputs = model.forward(inputs)
-        model.backward(inputs, outputs, labels, nesterov=False)
+        if params['loss_fn'] == 'ce':
+            model.backward(inputs, outputs, labels, nesterov=False, loss='ce')
+            loss = model.ce(outputs, labels)
+        elif params['loss_fn'] == 'mse':
+            model.backward(inputs, outputs, labels, nesterov=False, loss='mse')
+            loss = model.mse(outputs, labels)
+        else:
+            raise ValueError(f"No such loss function: {params['loss_fn']}")
         model.update(lr=params['lr'], gamma=params['momentum'])
+        loss_recorder.append(loss)
         if e % params['show_period'] == 0:
             preds = model.pred(outputs)
-            loss = model.mse(outputs, labels).mean()
             print(f"[{e:^7d}]Accuracy now is "
                   f"{model.cal_acc(preds, labels):.2%}"
                   f", Loss now is {loss:.2f}",)
-            if model.cal_acc(preds, labels) == 1:
-                print("Achieved 100% accuracy")
+            if loss < 0.01 or model.cal_acc(preds, labels) == 1:
+                print("Model has almost fitted the data")
+                print(f"Model prediction:\n {outputs}")
+                utils.show_result(inputs, labels, preds)
+                utils.show_loss_rec(loss_recorder, range(1, e+1))
                 break
 
 
@@ -139,6 +170,10 @@ def param_loader():
                         help="Gamma value in momentum")
     parser.add_argument("--show-period", type=int, default=100,
                         help="Period to show training result")
+    parser.add_argument("--data", type=str, choices=['linear', 'xor'],
+                        default='linear', help="Input data type")
+    parser.add_argument("--loss_fn", type=str, choices=['ce', 'mse'],
+                        default='ce', help="Choose your loss function")
     args, _ = parser.parse_known_args()
 
     return vars(args)
--- lab1/utils.py
+++ lab1/utils.py
@@ -31,14 +31,31 @@ def generate_xor_easy():
     return np.array(inputs), np.array(labels).reshape(21, 1)
 
 
-def show_result(x, y, pred_y=[]):
-    # plt.subplot(1, 2, 1)
+def show_result(x, y, pred_y):
+    plt.subplot(1, 2, 1)
     plt.title('Ground Truth', fontsize=18)
     for i in range(x.shape[0]):
         if y[i] == 0:
             plt.plot(x[i][0], x[i][1], 'ro')
         else:
             plt.plot(x[i][0], x[i][1], 'bo')
+
+    plt.subplot(1, 2, 2)
+    plt.title('Predict Result', fontsize=18)
+    for i in range(x.shape[0]):
+        if pred_y[i] == 0:
+            plt.plot(x[i][0], x[i][1], 'ro')
+        else:
+            plt.plot(x[i][0], x[i][1], 'bo')
+
+    plt.show(block=False)
+
+
+def show_loss_rec(loss, epoch):
+    plt.figure()
+    plt.title('Loss Record', fontsize=18)
+    plt.plot(epoch, loss)
+    plt.show()
 
 
 def main():
